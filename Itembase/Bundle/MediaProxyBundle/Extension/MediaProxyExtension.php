397a1ad72d8e20b35f4807260350a05fc01523f1
grandbora <bora.tunca@yahoo.com>
2013-01-15 01:28:51 +0100

    twig extension refactored

--- Itembase/Bundle/MediaProxyBundle/Extension/MediaProxyExtension.php
+++ Itembase/Bundle/MediaProxyBundle/Extension/MediaProxyExtension.php
@@ -1,67 +1,68 @@
 <?php
-
 namespace Itembase\Bundle\MediaProxyBundle\Extension;
 
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Itembase MediaProxyExtension
- *
  * A twig extension to generate and use a proxy path
  *
- * @package default
- * @author Thomas Bretzke, Itembase GmbH
+ * @author Thomas Bretzke <tb@itembase.biz>, Bora Tunca <bt@itembase.biz>
  **/
 class MediaProxyExtension extends \Twig_Extension {
 
+	private $prefixPath;
+	private $ignoreHttps;
+	private $algorithm;
+	private $secret;
 	private $router;
-	private $container;
 
-	public function __construct(UrlGeneratorInterface $router, $container)
+    /**
+     *
+     * @param string $prefixPath
+     * @param string $ignoreHttps
+     * @param string $algorithm
+     * @param string $secret
+     * @param UrlGeneratorInterface $router
+     */
+	public function __construct($prefixPath, $ignoreHttps, $algorithm, $secret, UrlGeneratorInterface $router)
 	{
+		$this->prefixPath = $prefixPath;
+		$this->ignoreHttps = $ignoreHttps;
+		$this->algorithm = $algorithm;
+		$this->secret = $secret;
 		$this->router = $router;
-		$this->container = $container;
 	}
 
-	public function getFilters() {
+	public function getFilters()
+	{
 		return array(
 			'ib_proxy_url'  => new \Twig_Filter_Method($this, 'proxyUrl'),
 		);
 	}
 
-	public function proxyUrl($url) {
+	public function proxyUrl($url) 
+	{
 		$parsedUrl = parse_url($url);
-		$secured = false;
 
-		if ( (!empty($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ) {
-			$secured = true;
-		}
+		if (false === array_key_exists('scheme', $parsedUrl)) { return $this->prefixPath.$url; }
 
-		// Has scheme
-		if (array_key_exists('scheme', $parsedUrl)) {
-			// If we have to ignore https, we have to pre check the protocol
-			if ($this->container->getParameter('ib_media_proxy.ignore_https')) {
-				if ($parsedUrl['scheme'] == 'https') {
-					return $url;
-				}
-			}
-		} else {
-			// If there's no scheme then add prefix
-			$prefixPath = $this->container->getParameter('ib_media_proxy.prefix_path');
-			return $this->container->getParameter('ib_media_proxy.prefix_path').$url;
-		}
-		if ($secured == true) {
-			// Generate proxy path
-			$hash = hash_hmac($this->container->getParameter('ib_media_proxy.algorithm'), $url, $this->container->getParameter('ib_media_proxy.secret'));
-			return $this->router->generate('ItembaseMediaProxyBundle_proxy', array('hash' => urlencode($hash))).'?path='.rawurlencode($url);			
-		} else {
+		if ($this->ignoreHttps && 'https' === $parsedUrl['scheme']) { // when we ignore https, we have to pre check the protocol
 			return $url;
 		}
+
+		$isSecure = (!empty($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443);
+		if (false === $isSecure) {
+			return $url;
+		}
+
+		// Generate proxy path
+		$hash = hash_hmac($this->algorithm, $url, $this->secret);
+		return $this->router->generate('ItembaseMediaProxyBundle_proxy', array('hash' => urlencode($hash), 'path' => rawurlencode($url)));
 	}
 
 	public function getName()
 	{
 		return 'ib_media_proxy';
 	}
-
 }
